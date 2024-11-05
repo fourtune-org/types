4856b0b33e9826a1868205f337ba43779c5211b0
eXory2024 <exory@anio.software>
2024-11-05 18:36:05 +0100

    Rename base-realm to core.

--- checkDefinitionFiles.sh
+++ checkDefinitionFiles.sh
@@ -8,7 +8,7 @@ function tsc() {
 	                         "$1"
 }
 
-tsc "./src/base-realm/v0/index.d.mts"
+tsc "./src/core/v0/index.d.mts"
 tsc "./src/base-realm-js-and-web/v0/index.d.mts"
 tsc "./src/realm-js-and-web/runtime/v0/index.d.mts"
 tsc "./src/realm-js-and-web/project/v0/index.d.mts"
